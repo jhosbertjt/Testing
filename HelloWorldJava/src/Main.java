9436e6983bccf48df8dd2fe2d4a02489da11361c
jhosbertjt <82987202+jhosbertjt@users.noreply.github.com>
2022-09-06 12:03:51 -0500

    Update Main.java

--- HelloWorldJava/src/Main.java
+++ HelloWorldJava/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Cambio logrado desde IntelliJ!");
+        System.out.println("Cambio logrado desde La Nube!");
     }
 }
