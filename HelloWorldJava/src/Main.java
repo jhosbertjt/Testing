668a6d7125c1ed55f303d16b8f98011666273211
jhosbertjt <82987202+jhosbertjt@users.noreply.github.com>
2022-09-06 12:09:32 -0500

    Cambio desde IntelliJ

--- HelloWorldJava/src/Main.java
+++ HelloWorldJava/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Cambioo logrado desde IntelliJ!");
-        System.out.println("Segundo cambio logrado desde IntelliJ!");
+        System.out.println("Cambio logrado desde IntelliJ222!");
         System.out.println("Cambio logrado desde la nube!");
     }
 }
